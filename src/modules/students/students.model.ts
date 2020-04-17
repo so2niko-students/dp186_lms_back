1d38f7aa3da781c5f888cd0464f894fd855e9334
Irina Giruta <irina.giruta@gmail.com>
2020-04-17 14:38:08 +0300

    fix commits

--- src/modules/students/students.model.ts
+++ src/modules/students/students.model.ts
@@ -1,33 +1,17 @@
-import { DataTypes, Model, ModelCtor, Sequelize } from 'sequelize';
+import { DataTypes, Model, Sequelize } from 'sequelize';
 
-export interface IStudent {
-    id?: number;
-    first_name_ukr: string;
-    last_name_ukr: string;
-    first_name_eng: string;
-    last_name_eng: string;
-    email: string;
-    phone_number: number;
-    password: string;
-    group_id: number;
-    createdAt?: Date;
-    updatedAt?: Date;
-}
+export class Student extends Model {
 
-export class Student extends Model implements IStudent {
-    // public static readonly ModelName: string = 'Student';
-    // public static readonly ModelNamePlural: string = 'Students';
-    public static readonly TableName: string = 'Students';
-
+    public static readonly TableName: string = 'students';
     public id!: number;
-    public first_name_ukr: string;
-    public last_name_ukr: string;
-    public first_name_eng: string;
-    public last_name_eng: string;
+    public firstNameUkr: string;
+    public lastNameUkr: string;
+    public firstNameEng: string;
+    public lastNameEng: string;
     public email: string;
-    public phone_number: number;
+    public phoneNumber: string;
     public password: string;
-    public group_id: number;
+    public groupId: number;
     public createdAt: Date;
     public updatedAt: Date;
 
@@ -42,18 +26,18 @@ export class Student extends Model implements IStudent {
                     allowNull: false,
                     autoIncrement: true,
                 },
-                first_name_ukr: new DataTypes.STRING(),
-                last_name_ukr: new DataTypes.STRING(),
-                first_name_eng: new DataTypes.STRING(),
-                last_name_eng: new DataTypes.STRING(),
-                email: new DataTypes.STRING(),
-                phone_number: new DataTypes.DOUBLE(),
-                password: new DataTypes.STRING(),
-                group_id: new DataTypes.INTEGER()
+                firstNameUkr: DataTypes.STRING(),
+                lastNameUkr: DataTypes.STRING(),
+                firstNameEng: DataTypes.STRING(),
+                lastNameEng: DataTypes.STRING(),
+                email: DataTypes.STRING(),
+                phoneNumber: DataTypes.STRING(),
+                password: DataTypes.STRING(),
+                groupId: DataTypes.INTEGER()
             }, {
                 sequelize: sequelize,
                 tableName: this.TableName,
             }
-        );
-    }
+        )
+    };
 }
