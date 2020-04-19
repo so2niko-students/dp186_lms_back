54085f0caa7d307603e81e138caba78fc86c36d2
Anton Panchenko <a.a.panchenkoo@gmail.com>
2020-04-19 20:53:50 +0300

    delete build folder

--- build/common/middlewares/errors.middleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = (err, req, res, next) => {
-    res.statusCode = err.statusCode;
-    res.send(err);
-};
-//# sourceMappingURL=errors.middleware.js.map
