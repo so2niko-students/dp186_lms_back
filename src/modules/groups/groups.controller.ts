399d435c38922869b8cb4be9e6eb603cc984cb93
Anton Panchenko <a.a.panchenkoo@gmail.com>
2020-04-23 12:00:21 +0300

    fixed by comments

--- src/modules/groups/groups.controller.ts
+++ src/modules/groups/groups.controller.ts
@@ -2,12 +2,13 @@ import { Response, NextFunction} from 'express';
 import { Groups as Group } from './groups.model';
 import { groupsService } from './groups.service';
 import {AuthRequest} from '../../common/types/types';
-
+import {BadRequest} from '../../common/exeptions';
 
 class GroupsController {
     public async createOne(req: AuthRequest, res: Response, next: NextFunction): Promise<void> {
         try {
             const group: Group = await groupsService.createOne(req.body, req.user);
+            res.statusCode = 201;
             res.send(group);
         } catch (e) {
             next(e);
@@ -15,6 +16,7 @@ class GroupsController {
     }
     public async findOne(req: AuthRequest, res: Response, next: NextFunction): Promise<void> {
         try {
+            this.checkIsNumber(req.params.id);
             const group: Group = await groupsService.findOne(+req.params.id, req.user);
             res.send(group);
         } catch (e) {
@@ -23,6 +25,7 @@ class GroupsController {
     }
     public async updateOne(req: AuthRequest, res: Response, next: NextFunction): Promise<void> {
         try {
+            this.checkIsNumber(req.params.id);
             const group: Group = await groupsService.updateOne(+req.params.id, req.body, req.user);
             res.send(group);
         } catch (e) {
@@ -31,6 +34,7 @@ class GroupsController {
     }
     public async deleteOne(req: AuthRequest, res: Response, next: NextFunction): Promise<void> {
         try {
+            this.checkIsNumber(req.params.id);
             const group: Group = await groupsService.deleteOne(+req.params.id, req.user);
             res.send(group);
         } catch (e) {
@@ -43,6 +47,11 @@ class GroupsController {
             res.send(groups);
         } catch (e) {
             next(e);
+        }
+    }
+    private checkIsNumber(num: any): void {
+        if (isNaN(+num)) {
+           throw new BadRequest('Incorrect group ID');
         }
     }
 }
--- src/modules/groups/groups.service.ts
+++ src/modules/groups/groups.service.ts
@@ -1,26 +1,35 @@
 import { Groups } from './groups.model';
-import { Teachers } from '../teachers/teachers.model';
-import { Students } from '../students/students.model';
 import { NotFound, BadRequest, Unauthorized } from '../../common/exeptions/';
 import { hashSync, genSaltSync } from 'bcrypt';
 import {CustomUser} from '../../common/types/types';
+import {teachersService} from '../teachers/teachers.service';
 
 interface IGroupCreate {
     groupName: string;
-    user: Teachers | Students;
+    teacherId?: number;
     groupToken?: string;
 }
 
 class GroupsService {
     public async createOne(data: IGroupCreate, user: CustomUser) {
-        await this.mentorVerification(user);
-        const { groupName } = data;
-        const group = await Groups.findOne( { where: {groupName, teacherId: data.user.id}} );
+        const mentor = await this.mentorVerification(user);
+        const { groupName, teacherId } = data;
+        const group = await Groups.findOne( { where: {groupName}} );
         if (group) {
-            throw new BadRequest(`Group with name "${groupName} already exist`);
+            throw new BadRequest(`Group with name "${groupName} already exists`);
+        }
+        if (teacherId && !mentor.isAdmin) {
+            throw new Unauthorized('You do not have the right to establish another teacher as a mentor of a new group.');
         }
         data.groupToken = await this.createGroupToken(groupName);
-        return Groups.create({...data});
+        if (teacherId) {
+            const teacher = await teachersService.findOneById(teacherId);
+            if (!teacher) {
+                throw new BadRequest(`Teacher with id "${teacherId} not found`);
+            }
+            return Groups.create({groupName, groupToken: data.groupToken, teacherId});
+        }
+        return Groups.create({groupName,  groupToken: data.groupToken});
     }
     public async findOne(id: number, user: CustomUser) {
         if (user.isMentor) {
@@ -38,10 +47,13 @@ class GroupsService {
         }
         return group;
     }
-    public async updateOne(id: number, data: object, user: CustomUser) {
+    public async updateOne(id: number, data: Groups, user: CustomUser) {
         const mentor = await this.mentorVerification(user);
         const group = await this.isGroupAvailable(id);
-        if (group.teacherId !== mentor.id) {
+        if (group.teacherId !== mentor.id && !mentor.isAdmin) {
+            throw new Unauthorized('You do not have rights to do this.');
+        }
+        if (data.teacherId && !mentor.isAdmin) {
             throw new Unauthorized('You do not have rights to do this.');
         }
         Object.keys(data).forEach((k) => group[k] = data[k]);
@@ -51,17 +63,18 @@ class GroupsService {
     public async deleteOne(id: number, user: CustomUser) {
         const mentor = await this.mentorVerification(user);
         const group = await this.isGroupAvailable(id);
-        if (group.teacherId !== mentor.id) {
+        if (group.teacherId !== mentor.id && !mentor.isAdmin) {
             throw new Unauthorized('You do not have rights to do this.');
         }
         group.destroy();
         return group;
     }
     public async findMany(user: CustomUser) {
-        await this.mentorVerification(user);
+        if (!user.isMentor) {
+            return Groups.findAll({ where: {id: user.groupId} });
+        }
         return Groups.findAll();
     }
-
     private async createGroupToken(name: string): Promise<string> {
         const salt = genSaltSync(5, 'b');
         const hash = hashSync(name, salt);
