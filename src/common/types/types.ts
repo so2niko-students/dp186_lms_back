d1675b06543b00a817d7f29aed3761c81c04a9f7
OksanaKozhukh <oksana.kozhukh71090@gmail.com>
2020-04-26 22:58:56 +0300

    merge types

--- src/common/types/types.ts
+++ src/common/types/types.ts
@@ -2,7 +2,8 @@ import {Request} from 'express';
 import {Teachers} from '../../modules/teachers/teachers.model';
 import {Students} from '../../modules/students/students.model';
 
-export type CustomUser = (Teachers  | Students) & {isMentor: boolean; groupId?: number; isAdmin?: boolean};
+export type CustomUser = (Teachers  | Students) & {isMentor: boolean;
+    groupId?: number; isAdmin?: boolean};
 export type AuthRequest = Request & {user: CustomUser};
 
 export type UpdateRequest<T>= Request & {user: T};
