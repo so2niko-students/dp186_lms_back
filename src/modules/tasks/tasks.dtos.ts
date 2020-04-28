b73346811fab9d42a1e3fa4dedc09193745e880d
IselDrew <andryshonochek0604@gmail.com>
2020-04-28 18:36:48 +0300

    Add uri to dto

--- src/modules/tasks/tasks.dtos.ts
+++ src/modules/tasks/tasks.dtos.ts
@@ -3,11 +3,11 @@ import * as Joi from 'joi';
 export const createTaskDto = Joi.object().keys({
   groupId: Joi.number().required(),
   taskName: Joi.string().min(2).required(),
-  fileURL: Joi.string(),
+  fileURL: Joi.string().uri(),
 });
 
 export const updateTaskDto = Joi.object().keys({
   groupId: Joi.number(),
   taskName: Joi.string().min(2),
-  fileURL: Joi.string(),
+  fileURL: Joi.string().uri(),
 });
