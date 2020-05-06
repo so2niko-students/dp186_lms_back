68ce27fabeff4122545e2b87b05b8372d739252c
IselDrew <andryshonochek0604@gmail.com>
2020-05-06 08:35:31 +0300

    Update tables

---
+++ src/migrations/20200506045647-remove-url-tasks.ts
@@ -0,0 +1,15 @@
+import { QueryInterface, DataTypes } from 'sequelize';
+import { Tasks } from '../modules/tasks/tasks.model';
+
+export async function up(query: QueryInterface) {
+  return query.removeColumn(Tasks.tableName, 'fileURL');
+}
+
+export async function down(query: QueryInterface) {
+    return query.addColumn(Tasks.tableName, 'fileURL', {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        comment: 'URL of the task',
+    });
+}
+
